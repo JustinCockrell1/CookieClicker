4d8e186c72f5defb585f1d5006635321879c39bc
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2024-08-10 07:11:05 -0500

    set up tailwind and add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+## Shop Items
+
+# Child
+Cost: 50
+Income: 1/s
+Description: "Convert a faithful child and receive his daily prayers"
+
+
+# Pastor
+Cost: 500
+Income 5/s
+Description: "Yadadada"
+
+# Prayer Upgrades
+More blessings per prayer
+
+##### Sin button
+
+
+# Multiplier Upgrades
+Multiply total amounts
+
+
+
+## Achievements
+
+# The game has begun
+Description "You purchased your first child"
+Happens when you purchase your first child
+
+# You're very faithful
+
+# 
--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 let count = 0;
 let targetValue = 0;
 
+let lastElapsedTime = 0;
+
+let totalDiff = 0;
+
 document.getElementById("button").onclick = function() {
-    targetValue += 100;
+    targetValue += 1;
     if(count!=targetValue) {
-        animateCount();
+        totalDiff = targetValue - count;
+        window.requestAnimationFrame(animateCount)
     }
 }
 
-function animateCount() {
+function animateCount(totalElapsedTime) {
 
-    if(count!=targetValue) {
+    const elapsedTime = totalElapsedTime - lastElapsedTime;
+    lastElapsedTime = totalElapsedTime;
+
+    count += totalDiff / elapsedTime;
+
+    if(count<targetValue) {
         window.requestAnimationFrame(animateCount);
     }
+    else if(count > targetValue) {
+        count = targetValue;
+    }
+
+    document.getElementById("count").innerHTML = count.toFixed(0);
 }
