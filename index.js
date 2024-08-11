50b4bd8ff7648ac2fef1397f2d517137e06f5b56
Nathan <nathanbc55@gmail.com>
2024-08-11 18:17:29 -0500

    parsenumber not working yet

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ game.addShopItem(
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Youth Leader", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Youth Leader", 500, 5, "./images/youthleader.webp")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Work Friends", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Work Friends", 500, 5, "./images/workfriends.png")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Bible Group", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Bible Group", 500, 5, "./images/biblegroup.png")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Prayer Hotline", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Prayer Hotline", 500, 5, "./images/hotline.jpeg")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
@@ -30,46 +30,98 @@ game.addShopItem(
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Congregation", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Congregation", 500, 5, "./images/congregation.png")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Religion", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Religion", 500, 5, "./images/religion.jpeg")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Neighborhood", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Neighborhood", 500, 5, "./images/neighborhood.jpg")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Partisan", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Partisan", 500, 5, "./images/partisan.jpg")
 );
 game.addShopItem(
     document.getElementById("buildings"), 
-    new ShopItem("Intergalactic Servants", 500, 5, "./images/shoppastor.jpg")
+    new ShopItem("Intergalactic Servants", 500, 5, "./images/alien.jpg")
 );
 
 game.addShopItem(
     document.getElementById("upgrades"), 
-    new ShopUpgrade("Pastor","pastor",5,"./images/shopchild.png", (game)=>{
+    new ShopUpgrade("Kneel","Increases the amount per click",5,"./images/kneel.jpg", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Close your eyes","Increases the amount per click",5,"./images/closeeyes.jpg", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Raise your voice","Increases the amount per click",5,"./images/raise.jpg", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Shake stick","Increases the amount per click",5,"./images/stick.png", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Light Incense","Increases the amount per click",5,"./images/incense.jpg", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Prostrate","Increases the amount per click",5,"./images/prostrate.jpeg", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Tears","Increases the amount per click",5,"./images/tears.png", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Visit Temple","Increases the amount per click",5,"./images/temple.avif", (game)=>{
+        game.clickRate*=2;
+    })
+);
+game.addShopItem(
+    document.getElementById("upgrades"), 
+    new ShopUpgrade("Offer Sacrifice","Increases the amount per click",5,"./images/sacrifice.png", (game)=>{
         game.clickRate*=2;
     })
 );
 
-game.addShopItem(
-    document.getElementById("upgrades"), 
-    new ShopUpgrade("Click better","makes you click better",50,"./images/shopchild.png", (game)=>{
-        game.rate+=10;
-    })
-);
+function parseNumber(number) {
+    let word = "";
+    if(number > 999 && number < 1000000) {
+        number = number/1000;
+        word = "thousand";
+    };
+    number = number.toFixed(3);
+    let displayNumber = number + word;
+    return displayNumber;
+}
 
 clickerButton.onclick = function() {
     game.earn(game.clickRate);
-    document.getElementById("blessings").innerHTML = game.blessings;
+    document.getElementById("blessings").innerHTML = parseNumber(game.blessings);
 }
 
 window.setInterval(()=>{
 
     game.earn(game.rate);
-    document.getElementById("blessings").innerHTML = game.blessings;
+    document.getElementById("blessings").innerHTML = parseNumber(game.blessings);
 }, 1000)
